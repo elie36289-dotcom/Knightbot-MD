16de537de7a9f1e16b9fd531486ce3b0272c1db2
elie36289-dotcom <elie36289@gmail.com>
2026-01-11 00:53:06 +0000

    Change command mode from private to public

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const settings = {
   botOwner: 'BMEDIA', // Your name
   ownerNumber: '2250702479496', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "private",
+  commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
